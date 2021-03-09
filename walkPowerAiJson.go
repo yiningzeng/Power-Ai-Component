c4e7533762178ee8cae177de6e7f5c5452d6e4e2
baymin <yiningzeng@outlook.com>
2021-03-09 14:45:16 +0800

    feat: 优化处理后的数据排序问题+总数统计错误修复

--- walkPowerAiJson.go
+++ walkPowerAiJson.go
@@ -14,6 +14,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"path"
 	"runtime"
 	"strconv"
 	"strings"
@@ -25,28 +26,46 @@ import (
 var startNum int
 var dirPath string
 var threadNum int
-var version = "2.2.2"
-var m cmap.Cmap
+var version = "3.3.3"
+var assetsVisited cmap.Cmap
+var assetsTagged cmap.Cmap
+var assetsNotVisited cmap.Cmap
 var tagsMap cmap.Cmap
 
-func read(fs afero.Fs, path string, pool *gpool.Pool) {
+/*
+   NotVisited = 0,
+   Visited = 1,
+   Tagged = 2,*/
+func read(fs afero.Fs, filePath string, pool *gpool.Pool) {
 	defer pool.Done()
-	b, err := afero.ReadFile(fs, path)
+	bytes, err := afero.ReadFile(fs, filePath)
 	if err != nil {
-		logrus.WithField("file", path).Error(err.Error())
+		logrus.WithField("file", filePath).Error(err.Error())
 	}
 	var ki PowerAiAsset
-	err = json.Unmarshal(b,  &ki)
+	err = json.Unmarshal(bytes,  &ki)
 	if err != nil {
-		logrus.WithField("file", path).Error(err.Error())
+		logrus.WithField("file", filePath).Error(err.Error())
 	}
 	if ki.Asset.Name != "" {
-		ki.Asset.Path = "file:" + dirPath + "/" + ki.Asset.Name
+		ki.Asset.Path = "file:" + path.Join(dirPath, ki.Asset.Name)
 		assetJson, err2 := json.Marshal(ki.Asset)
 		if err2 != nil {
-			logrus.WithField("file", path).Error(err2.Error())
+			logrus.WithField("file", filePath).Error(err2.Error())
 		}
-		m.Store(ki.Asset.Id, "\"" + ki.Asset.Id +"\":" + string(assetJson))
+		switch ki.Asset.State {
+			case 0: // 未查看的数据
+				assetsNotVisited.Store(ki.Asset.Id, "\"" + ki.Asset.Id +"\":" + string(assetJson))
+				break
+			case 1: // 已查看的数据
+				assetsVisited.Store(ki.Asset.Id, "\"" + ki.Asset.Id +"\":" + string(assetJson))
+				break
+			case 2: // 已标记的数据
+				assetsTagged.Store(ki.Asset.Id, "\"" + ki.Asset.Id +"\":" + string(assetJson))
+				break
+			default:
+				break
+		}
 		if ki.Asset.Tags != "" {
 			for _, v := range strings.Split(ki.Asset.Tags, ",") {
 				if _, ok := tagsMap.Load(v); !ok {
@@ -64,20 +83,19 @@ func read(fs afero.Fs, path string, pool *gpool.Pool) {
 		}*/
 	}
 	startNum++
-	if startNum%20 == 0 {
+	if startNum%10 == 0 {
+		runtime.GC()
 		pool.Add(1)
 		go func(p *gpool.Pool) {
 			defer p.Done()
-			ssss := exec.Command("/bin/bash", "-c", "echo " + strconv.Itoa(startNum) + " > now.txt")
-			ssss.Run()
+			exec.Command("/bin/bash", "-c", "echo " + strconv.Itoa(startNum) + " > now.txt").Run()
 		}(pool)
 	}
-	//logrus.WithField("id", n).Info(ki.Asset.Name)
 }
 
 //绑定参数变量
 func flagInit() {
-	flag.StringVar(&dirPath, "path", ".", "the path must be input")
+	flag.StringVar(&dirPath, "path", "/home/baymin/daily-work/data/NG-12.19/2020-12-18", "the path must be input")
 	flag.IntVar(&threadNum, "tn", 100, "the default threadNum is 100")
 	flag.StringVar(&version, "v", version, "the version of monkeySun")
 	flag.Usage = func() {
@@ -117,72 +135,51 @@ func LoggerInit(debug bool) {
 
 func main() {
 	flagInit()
-	LoggerInit(false)
+	LoggerInit(true)
 	flag.Parse()
-	allTags := [...]string{"#FFB6C1","#FFC0CB","#DC143C","#FFF0F5","#DB7093","#FF69B4","#FF1493","#C71585","#DA70D6","#D8BFD8","#DDA0DD","#EE82EE","#FF00FF","#FF00FF","#8B008B","#800080","#BA55D3","#9400D3","#9932CC","#4B0082","#8A2BE2","#9370DB","#7B68EE","#6A5ACD","#483D8B","#E6E6FA","#F8F8FF","#0000FF","#0000CD","#191970","#00008B","#000080","#4169E1","#6495ED","#B0C4DE","#778899","#708090","#1E90FF","#F0F8FF","#4682B4","#87CEFA","#87CEEB","#00BFFF","#ADD8E6","#B0E0E6","#5F9EA0","#F0FFFF","#E1FFFF","#AFEEEE","#00FFFF","#D4F2E7","#00CED1","#2F4F4F","#008B8B","#008080","#48D1CC","#20B2AA","#40E0D0","#7FFFAA","#00FA9A","#00FF7F","#F5FFFA","#3CB371","#2E8B57","#F0FFF0","#90EE90","#98FB98","#8FBC8F","#32CD32","#00FF00","#228B22","#008000","#006400","#7FFF00","#7CFC00","#ADFF2F","#556B2F","#F5F5DC","#FAFAD2","#FFFFF0","#FFFFE0","#FFFF00","#808000","#BDB76B","#FFFACD","#EEE8AA","#F0E68C","#FFD700","#FFF8DC","#DAA520","#FFFAF0","#FDF5E6","#F5DEB3","#FFE4B5","#FFA500","#FFEFD5","#FFEBCD","#FFDEAD","#FAEBD7","#D2B48C","#DEB887","#FFE4C4","#FF8C00","#FAF0E6","#CD853F","#FFDAB9","#F4A460","#D2691E","#8B4513","#FFF5EE","#A0522D","#FFA07A","#FF7F50","#FF4500","#E9967A","#FF6347","#FFE4E1","#FA8072","#FFFAFA","#F08080","#BC8F8F","#CD5C5C","#FF0000","#A52A2A","#B22222","#8B0000","#800000"}
+	var allTags []string
+	if bytes, err := afero.ReadFile(afero.NewOsFs(), "colorsTemplate.json"); err == nil {
+		if err = json.Unmarshal(bytes, &allTags); err != nil {
+			logrus.Error(err)
+			allTags = []string{"#FF3399", "#FF66FF","#99CCFF", "#33FF33", "#FFFFF", "#FFCC00", "#FFFF00", "#660099", "#0033FF", "#FF0033"}
+		}
+	} else {
+		allTags = []string{"#FF3399", "#FF66FF","#99CCFF", "#33FF33", "#FFFFF", "#FFCC00", "#FFFF00", "#660099", "#0033FF", "#FF0033"}
+	}
 
-	//for i:=0; i<500000; i++ {
-	//	f, _:=os.Open("../fucker/a.json")
-	//	defer f.Close()
-	//	tar, _ := os.Create("../fucker/" + strconv.Itoa(i) + ".json")
-	//	defer tar.Close()
-	//	io.Copy(tar, f)
-	//}
-
-	// 定义几个变量，用于接收命令行的参数值
-	//var path       string
-	//var threadNum    int
-	//var host        string
-	//var port        int
-	//// &user 就是接收命令行中输入 -u 后面的参数值，其他同理
-	//flag.StringVar(&user, "u", "root", "账号，默认为root")
-	//flag.StringVar(&password, "p", "", "密码，默认为空")
-	//flag.StringVar(&host, "h", "localhost", "主机名，默认为localhost")
-	//flag.IntVar(&port, "P", 3306, "端口号，默认为3306")
 	exec.Command("/bin/bash", "-c", "echo 0 > now.txt").Run()
 	start := time.Now()
 	pool := gpool.New(threadNum)
 	fs := afero.NewOsFs()
+	// 由于linux上加载的远程文件夹遇到大量数据的情况下无法使用遍历文件的方法，只能先使用ls命令写入本地数据再处理
 	cmd := exec.Command("/bin/sh", "-c", "ls '" + dirPath + "' |grep .json > jsonList.txt")
 	_, err := cmd.Output()
-	if err!=nil{
+	if err != nil {
 		logrus.Error(err)
 	}
 
+	if bytes, err := afero.ReadFile(fs, "jsonList.txt"); err == nil {
+		allNum := strconv.Itoa(len(strings.Split(string(bytes), "\n")))
+		_ = afero.WriteFile(fs,  "allNum.txt",  []byte(allNum), 0755)
+	}
+
 	cc, _ := os.Open("jsonList.txt")
-	defer cc.Close()
 	br := bufio.NewReader(cc)
 	for {
 		a, _, c := br.ReadLine()
 		if c == io.EOF {
 			break
 		}
-		//logrus.Info(startNum)
 		pool.Add(1)
-		go read(fs, dirPath + "/" + string(a), pool)
+		go read(fs, path.Join(dirPath, string(a)), pool)
 	}
-
-
-
-	//_ = afero.Walk(fs, dirPath, func(path string, info os.FileInfo, err error) error{
-	//	if strings.Contains(info.Name(), ".json") == true {
-	//		n++
-	//		logrus.Info(info.Name())
-	//		//if n > 19000 {
-	//		//	return nil
-	//		//} else {
-	//			pool.Add(1)
-	//			go read(fs, path, strconv.Itoa(n), pool)
-	//		//}
-	//	}
-	//	//logrus.WithField("n", n).Info(info.Name())
-	//	return nil
-	//})
 	pool.Wait()
+	cc.Close()
 	var mutex sync.Mutex
 	fp := gpool.New(threadNum)
 	projectJson := "{\"assets\": {"
-	m.Range(func(key, value interface{}) bool {
+
+	assetsTagged.Range(func(key, value interface{}) bool {
 		fp.Add(1)
 		go func(s string, pool2 *gpool.Pool) {
 			runtime.GC()
@@ -194,6 +191,33 @@ func main() {
 		return true
 	})
 	fp.Wait()
+
+	assetsVisited.Range(func(key, value interface{}) bool {
+		fp.Add(1)
+		go func(s string, pool2 *gpool.Pool) {
+			runtime.GC()
+			mutex.Lock()
+			projectJson += s + ","
+			mutex.Unlock()
+			pool2.Done()
+		}(value.(string), fp)
+		return true
+	})
+	fp.Wait()
+
+	assetsNotVisited.Range(func(key, value interface{}) bool {
+		fp.Add(1)
+		go func(s string, pool2 *gpool.Pool) {
+			runtime.GC()
+			mutex.Lock()
+			projectJson += s + ","
+			mutex.Unlock()
+			pool2.Done()
+		}(value.(string), fp)
+		return true
+	})
+	fp.Wait()
+
 	projectJson = strings.TrimRight(projectJson, ",")
 	projectJson += "}}"
 	//logrus.Info("开始写入")
@@ -209,7 +233,7 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	var tagsFinal []PowerAiTags
 	tagsMap.Range(func(key, value interface{}) bool {
-		tags := PowerAiTags{Name: value.(string), Color: allTags[rand.Intn(127)]}
+		tags := PowerAiTags{Name: value.(string), Color: allTags[rand.Intn(len(allTags) - 1)]}
 		tagsFinal = append(tagsFinal, tags)
 		//logrus.WithField("color", allTags[rand.Intn(127)]).Error(value.(string))
 		return true
